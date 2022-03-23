8f8385ffe5bf7b76e81688ea5f36b8972749870b
Bornord <best.enguerran@gmail.com>
2022-03-23 07:52:15 +0100

    test

--- Communication/frontend/src/App.vue
+++ Communication/frontend/src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <div id="app">
+  <div id="app" class="wrapper">
+
+
     <nav>
+      <div class="one">
       <router-link to="/">Home</router-link> |
       <router-link to="/comm">Requête simple</router-link> |
       <router-link to="/testSQL">Test avec SQL</router-link> |
       <router-link to="/ajoutPersonne">Ajout de personne</router-link> | 
       <router-link to="/ajoutAdresse">Ajout d'une adresse</router-link> | 
       <router-link to="/associer">Associer</router-link> |
-      <router-link to="/affichage">Affichage</router-link> 
+      <router-link to="/affichage">Affichage</router-link>
+      </div> 
+      <div class="two">
+        Test
+      </div>
     </nav>
     <router-view/>
     <!--
@@ -38,6 +45,7 @@ import HomeView from "./views/HomeView.vue";
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
 
 nav {
@@ -52,4 +60,22 @@ nav {
     }
   }
 }
+
+.wrapper {
+  display: grid;
+  // fr = pourcentage
+  grid-template-columns: repeat(1, 2fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+}
+.one {
+  grid-column: 1 / 3;
+  grid-row: 1;
+}
+.two {
+  grid-column: 2 / 4;
+  grid-row: 1;
+}
+
 </style>
+
